abb7e4f879c8ba4fb7a25f53d31dcced8e65c339
Zhao KK <kikakkz@hotmail.com>
2025-07-22 16:05:00 +0800

    Use TAG in the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/linera-io/linera-protocol/archive/refs/tags/v0.14.1.zip
-unzip v0.14.1.zip -d /app/data/inputs/linera-protocol-v0.14.1
+TAG=${TAG:-v0.14.1}
+
+wget https://github.com/linera-io/linera-protocol/archive/refs/tags/${TAG}.zip
+unzip ${TAG}.zip -d /app/data/inputs/linera-protocol-${TAG}
 
 python -m lightrag.api.lightrag_server
