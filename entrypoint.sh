0f6ebb8a4e3e4261397441cb73a34c549f5822d9
root <root@ip-172-31-41-137.eu-central-1.compute.internal>
2025-07-22 08:53:42 +0000

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ TAG=${TAG:-v0.14.1}
 
 wget https://github.com/linera-io/linera-protocol/archive/refs/tags/${TAG}.zip
 unzip ${TAG}.zip -d /app/data/inputs
+rm /app/data/inputs/linera-protocol-${TAG}/.github -rf
 
 python -m lightrag.api.lightrag_server --auto-scan-at-startup
