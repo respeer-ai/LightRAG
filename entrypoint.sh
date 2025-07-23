922b4d0981fb3d29f75edba6124eec7fd8cc461a
root <root@ip-172-31-20-225.eu-central-1.compute.internal>
2025-07-23 02:59:04 +0000

    Fix dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 TAG=${TAG:-v0.14.1}
 
+mkdir -p /app/data/inputs
 wget https://github.com/linera-io/linera-protocol/archive/refs/tags/${TAG}.zip
 unzip ${TAG}.zip -d /app/data/inputs
 rm /app/data/inputs/linera-protocol-${TAG}/.github -rf
